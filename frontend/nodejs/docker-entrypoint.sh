4183ea32c03e1b2339a18eea0cfd49fd34681c7a
A1exanderr <looveyouuu10@gmail.com>
2025-10-02 11:55:16 -0400

    desarrollo version mejorada

--- frontend/nodejs/docker-entrypoint.sh
+++ frontend/nodejs/docker-entrypoint.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
+# Si no existe angular.json, asumimos que no hay proyecto
 if [ ! -f "angular.json" ]; then
   echo "No se encontró un proyecto de Angular. Creando uno nuevo..."
   ng new my-app --directory=./ --skip-git --skip-install --strict=false
+  # Instalar dependencias del nuevo proyecto
+  #npm install
 fi
-
-# Instalar dependencias solo si node_modules no existe
-if [ ! -d "node_modules" ]; then
-  npm install
-fi
-
-# Ejecutar Angular dev server
+npm install
+# Ejecutar ng serve
 exec ng serve --host 0.0.0.0
