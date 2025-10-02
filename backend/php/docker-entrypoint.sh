c66e062f0084a35d7da2886954245d517fbcb6c3
A1exanderr <looveyouuu10@gmail.com>
2025-10-02 14:02:28 -0400

    imagenes listos para desarrollo desde cero

--- backend/php/docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-if ! command -v laravel >/dev/null 2>&1; then
-    composer global require laravel/installer
-    ln -s /root/.composer/vendor/bin/laravel /usr/local/bin/laravel
-fi
-# Iniciar PHP-FPM
-exec php-fpm
