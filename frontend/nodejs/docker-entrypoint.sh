840391126d1d144399bd504a4d9ab8c2d70fa480
A1exanderr <looveyouuu10@gmail.com>
2025-10-02 13:50:53 -0400

    imagenes listos para desarrollo desde cero

--- frontend/nodejs/docker-entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Si no existe angular.json, asumimos que no hay proyecto
-#if [ ! -f "angular.json" ]; then
-#  echo "No se encontró un proyecto de Angular. Creando uno nuevo..."
-#  ng new my-app --directory=./ --skip-git --skip-install --strict=false
-#fi
-echo "No se encontró un proyecto de Angular. Creando uno nuevo..."
